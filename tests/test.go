1418cccf96a5e5e2933c2f6385a14bafcf521465
Jesse the Game <j.k.zwaan@gmail.com>
2014-11-29 17:50:10 +0000

    Split off keydown

--- tests/test.go
+++ tests/test.go
@@ -3,15 +3,15 @@ package main
 import "github.com/jessethegame/colorgrid"
 
 func main() {
-	g := colorgrid.Grid{colorgrid.Size{5, 3}}
-	colorgrid.Clear()
+	g := colorgrid.Grid{colorgrid.Size{5, 3}, colorgrid.RED, colorgrid.GREEN}
+	g.Clear()
 	var x, y int
 	for y = 0; y < 8; y++ {
 		for x = 0; x < 8; x++ {
 			if (x%2 == 0 && y%2 == 0) || (x%2 == 1 && y%2 == 1) {
-				g.Render(x, y, 'X', colorgrid.BLACK, colorgrid.WHITE)
+				g.Print(x, y, "X", colorgrid.BLACK, colorgrid.WHITE)
 			} else {
-				g.Render(x, y, 'X', colorgrid.WHITE, colorgrid.BLACK)
+				g.Print(x, y, "X", colorgrid.WHITE, colorgrid.BLACK)
 			}
 		}
 	}
